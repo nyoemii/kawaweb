738ea30963179d9d12533cf727dd69977b52613d
ロキ | Loki <goingloki@gmail.com>
2023-12-20 22:54:53 -0500

    yay perms

--- objects/privileges.py
+++ objects/privileges.py
@@ -9,26 +9,45 @@ __all__ = ('Privileges',)
 class Privileges(IntFlag):
     """Server side user privileges."""
 
-    # privileges intended for all normal players.
-    Normal      = 1 << 0 # is an unbanned player.
-    Verified    = 1 << 1 # has logged in to the server in-game.
+    Banned              = 0        # user is banned
+    Normal              = 1        # user is not restricted.
+    Verified            = 2 << 0   # has logged in to the server in-game.
+    Supporter           = 2 << 1   # user is a supporter.
+    AccessPanel         = 2 << 2   # probably wont be used much.    
+    ManageUsers         = 2 << 3   # can manage users? probably going to be used for changing passwords/email/etc
+    BanUsers            = 2 << 4   # can ban users
+    SilenceUsers        = 2 << 5   # can silence users
+    WipeUsers           = 2 << 6   # can wipe users
+    ManageBeatmaps      = 2 << 7   # able to manage maps ranked status.
+    #ManageServers      = 2 << 8  
+    #ManageSettings     = 2 << 9  
+    #ManageBetaKeys     = 2 << 10  
+    #ManageReports      = 2 << 11  
+    #ManageDocs         = 2 << 12  
+    ManageBadges        = 2 << 13  # can manage badges
+    ViewPanelLog        = 2 << 14  # can view the panel log
+    ManagePrivs         = 2 << 15  # can manage privs of users
+    SendAlerts          = 2 << 16  # can send in-game alerts? probably not going to be used much
+    ChatMod             = 2 << 17  # chat mod, no way
+    KickUsers           = 2 << 18  # can kick users
+    #PendingVerify      = 2 << 19  # completely deprecated, unused. dont use this.
+    Tournament          = 2 << 20  # able to manage match state without host.
+    #Caker              = 2 << 21  # what is this??
+    ManageClans         = 2 << 27  # can manage clans.
+    ViewSensitiveInfo   = 2 << 28  # can view ips, hwids, disk ids of users. super awesome with the new system.
+    IsBot               = 2 << 30  # BOT_USER
+    Whitelisted         = 2 << 31  # has bypass to low-ceiling anticheat measures (trusted).
+    Premium             = 2 << 32  # 'premium' donor
+    Alumni              = 2 << 33  # notable users, receives some extra benefits.
+    Dangerous           = 2 << 34  # able to manage full server app.state.
 
-    # has bypass to low-ceiling anticheat measures (trusted).
-    Whitelisted = 1 << 2
 
-    # donation tiers, receives some extra benefits.
-    Supporter   = 1 << 4
-    Premium     = 1 << 5
 
-    # notable users, receives some extra benefits.
-    Alumni      = 1 << 7
 
-    # staff permissions, able to manage server state.
-    Tournament  = 1 << 10 # able to manage match state without host.
-    Nominator   = 1 << 11 # able to manage maps ranked status.
-    Mod         = 1 << 12 # able to manage users (level 1).
-    Admin       = 1 << 13 # able to manage users (level 2).
-    Dangerous   = 1 << 14 # able to manage full server state.
+    Nominator = ManageBeatmaps | AccessPanel
+    Mod = BanUsers | SilenceUsers | WipeUsers | KickUsers| ManagePrivs | ChatMod # define this as a moderator
+    Admin = Mod | ViewSensitiveInfo | ManageUsers  # has moderator privileges, can view sensitive info and manage users
+    
 
     Donator = Supporter | Premium
     Staff = Mod | Admin | Dangerous
