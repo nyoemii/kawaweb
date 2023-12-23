79b14677c8de1a7d6361576b4d5f443a6a3bea93
Austin Steyer <asteyer11@gmail.com>
2023-12-23 15:09:18 -0700

    Add profile overview panel to leaderboards.

--- static/js/pages/leaderboard.js
+++ static/js/pages/leaderboard.js
@@ -4,12 +4,12 @@ new Vue({
     data() {
         return {
             flags: window.flags,
-            boards : {},
-            mode : 'std',
-            mods : 'vn',
-            sort : 'pp',
-            load : false,
-            no_player : false, // soon
+            boards: {},
+            mode: 'std',
+            mods: 'vn',
+            sort: 'pp',
+            load: false,
+            no_player: false, // soon
         };
     },
     created() {
@@ -31,10 +31,12 @@ new Vue({
             this.$set(this, 'mods', mods);
             this.$set(this, 'sort', sort);
             this.$set(this, 'load', true);
-            this.$axios.get(`${window.location.protocol}//api.${domain}/v1/get_leaderboard`, { params: {
-                mode: this.StrtoGulagInt(),
-                sort: this.sort
-            }}).then(res => {
+            this.$axios.get(`${window.location.protocol}//api.${domain}/v1/get_leaderboard`, {
+                params: {
+                    mode: this.StrtoGulagInt(),
+                    sort: this.sort
+                }
+            }).then(res => {
                 this.boards = res.data.leaderboard;
                 this.$set(this, 'load', false);
             });
@@ -61,17 +63,69 @@ new Vue({
         },
         StrtoGulagInt() {
             switch (this.mode + "|" + this.mods) {
-                case 'std|vn': return 0;
-                case 'taiko|vn': return 1;
-                case 'catch|vn': return 2;
-                case 'mania|vn': return 3;
-                case 'std|rx': return 4;
-                case 'taiko|rx': return 5;
-                case 'catch|rx': return 6;
-                case 'std|ap': return 8;
-                default: return -1;
+                case 'std|vn':
+                    return 0;
+                case 'taiko|vn':
+                    return 1;
+                case 'catch|vn':
+                    return 2;
+                case 'mania|vn':
+                    return 3;
+                case 'std|rx':
+                    return 4;
+                case 'taiko|rx':
+                    return 5;
+                case 'catch|rx':
+                    return 6;
+                case 'std|ap':
+                    return 8;
+                default:
+                    return -1;
             }
         },
     },
-    computed: {}
 });
+Vue.component('user-profile', {
+    props: ['user'],
+    template: `
+      <span :id="user.player_id" class="user-name" @mouseover="showProfile" @mouseout="hideProfile">
+        <a :href="'/u/'+user.player_id+'?mode='+mode+'&mods='+mods">
+          {{ user.name }}
+        </a>
+        <div :id="user.player_id" class="profile-panel" v-bind:style="{ display: profileStyle }">
+          <div class="profile-panel-background" :style="'background-image: url(/backgrounds/' + user.player_id + ')'"></div>
+          <div class="profile-panel-avatar" :style="'background-image: url(https://a.' + domain + '/' + user.player_id + ')'"></div>
+          <div class="profile-panel-info">
+            <div class="name" >
+              <span v-if="user.clan_tag">
+                <a>
+                  [{{ user.clan_tag }}]
+                </a>
+              </span> 
+              {{ user.name }}
+            </div>
+            <div class="Badges">
+            </div>
+          </div>
+        </div>
+      </span>
+    `,
+    data: function() {
+      return {
+        profileVisible: false
+      };
+    },
+    methods: {
+      showProfile: function() {
+        this.profileVisible = true;
+      },
+      hideProfile: function() {
+        this.profileVisible = false;
+      }
+    },
+    computed: {
+        profileStyle: function() {
+            return this.profileVisible ? 'flex !important' : 'none !important';
+        }
+    }
+  });
