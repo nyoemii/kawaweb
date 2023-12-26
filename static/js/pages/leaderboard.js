87b1aa892803ab17ec36ddafa4222381f4c20f3c
Austin Steyer <asteyer11@gmail.com>
2023-12-26 12:57:13 -0700

    Fix and improve profile panel badge display on leaderboard.

--- static/js/pages/leaderboard.js
+++ static/js/pages/leaderboard.js
@@ -86,66 +86,69 @@ new Vue({
     },
 });
 Vue.component('user-profile', {
-        props: ['user'],
-        template: `
-        <span :id="user.player_id" class="user-name" @mouseover="showProfile" @mouseout="hideProfile">
-                <a :href="'/u/'+user.player_id+'?mode='+mode+'&mods='+mods">
-                        {{ user.name }}
-                </a>
-                <div :id="user.player_id" class="profile-panel" v-bind:style="{ display: profileStyle }">
-                        <div class="profile-panel-background" :style="'background-image: url(/backgrounds/' + user.player_id + ')'"></div>
-                        <div class="profile-panel-avatar" :style="'background-image: url(https://a.' + domain + '/' + user.player_id + ')'"></div>
-                        <div class="profile-panel-info">
-                        <div class="name" >
-                                <span v-if="user.clan_tag">
-                                        <a>
-                                                [{{ user.clan_tag }}]
-                                        </a>
-                                </span> 
-                                {{ user.name }}
-                        </div>
-                        <div class="Badges">
-                                <span v-for="badge in user.badges">
-                                        <i :class="'badge ' + badge.styles.icon" :style="'color: hsl(' + badge.styles.color + ', 80%, 80%);'" @mouseover="showBadgePopup(badge)" @mouseout="hideBadgePopup"></i>
-                                        <div class="badge-popup" v-if="badgePopupVisible" :style="{ top: badgePopupTop, left: badgePopupLeft }">
-                                                <div class="badge-name">{{ badge.name }}</div>
-                                                <div class="badge-description">{{ badge.description }}</div>
-                                        </div>
-                                </span>
-                        </div>
+    props: ['user'],
+    template: `
+    <span :id="user.player_id" class="user-name" @mouseover="showProfile" @mouseout="hideProfile">
+        <a :href="'/u/'+user.player_id+'?mode='+mode+'&mods='+mods">
+            {{ user.name }}
+        </a>
+        <div :id="user.player_id" class="profile-panel" v-bind:style="{ display: profileStyle }">
+            <div class="profile-panel-background" :style="'background-image: url(/backgrounds/' + user.player_id + ')'"></div>
+            <div class="profile-panel-avatar" :style="'background-image: url(https://a.' + domain + '/' + user.player_id + ')'"></div>
+            <div class="profile-panel-info">
+                <div class="name">
+                    <span v-if="user.clan_tag">
+                        <a>
+                            [{{ user.clan_tag }}]
+                        </a>
+                    </span>
+                    {{ user.name }}
                 </div>
-        </span>
-        `,
-        data: function() {
-            return {
-                profileVisible: false,
-                badgePopupVisible: false,
-                badgePopupTop: 0,
-                badgePopupLeft: 0,
-            };
+                <div class="Badges">
+                    <span v-for="badge in user.badges">
+                        <i :class="'badge ' + badge.styles.icon" :style="'color: hsl(' + badge.styles.color + ', 100%, 60%);'" @mouseover="showBadgePopup($event, badge)" @mouseout="hideBadgePopup"></i>
+                        <div class="badge-popup" v-if="badgePopupVisible == badge.id" :style="{ top: -20, left: 0 }">
+                            <div class="badge-name" :style="'color: hsl('+ badge.styles.color + ', 100%, 60%);'">{{ badge.name }}</div>
+                            <div class="badge-description">{{ badge.description }}</div>
+                        </div>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </span>
+    `,
+    data: function() {
+        return {
+            profileVisible: false,
+            badgePopupVisible: false,
+            badgePopupTop: 0,
+            badgePopupLeft: 0,
+        };
+    },
+    methods: {
+        showProfile: function() {
+            this.profileVisible = true;
         },
-        methods: {
-            showProfile: function() {
-                this.profileVisible = true;
-            },
-            hideProfile: function() {
-                this.profileVisible = false;
-            },
-            showBadgePopup: function(badge) {
-                this.badgePopupVisible = true;
+        hideProfile: function() {
+            this.profileVisible = false;
+        },
+        showBadgePopup: function(event, badge) {
+            if (this.badgePopupVisible != badge.id) {
+                this.badgePopupVisible = badge.id;
                 // Calculate the position of the badge popup relative to the badge icon
                 const badgeIcon = event.target;
                 const badgeIconRect = badgeIcon.getBoundingClientRect();
                 this.badgePopupTop = badgeIconRect.top + badgeIconRect.height + 'px';
                 this.badgePopupLeft = badgeIconRect.left + 'px';
-            },
-            hideBadgePopup: function() {
-                this.badgePopupVisible = true;
             }
-        },
-        computed: {
-                profileStyle: function() {
-                        return this.profileVisible ? 'flex !important' : 'none !important';
-                }
+            },
+        hideBadgePopup: function() {
+            this.badgePopupVisible = false;
         }
+    },
+    computed: {
+        profileStyle: function() {
+            return this.profileVisible ? 'flex !important' : 'none !important';
+        }
+    }
 });
