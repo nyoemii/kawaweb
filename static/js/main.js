e8652870ee2d9ed280ef2f92c748885487ddfcde
Austin Steyer <asteyer11@gmail.com>
2023-12-29 21:25:04 -0700

    Change CSS Var and Move panel stuff.

--- static/js/main.js
+++ static/js/main.js
@@ -16,3 +16,76 @@ function togglenavbar() {
     document.getElementById('navbar').classList.toggle("is-active");
     document.getElementById('navbar-burger').classList.toggle("is-active");
 }
+var panelBus = new Vue();
+new Vue({
+    el: "#panel",
+    delimiters: ["<%", "%>"],
+    data() {
+        return {
+            data: {
+                show: false,
+                panel: '',
+                panelData: {},
+            },
+        };
+    },
+    async created() {
+        panelBus.$on('show-score-window', (scoreId) => { 
+            // Reset Score Window Data
+            this.data = {
+                show: true,
+                panel: 'Score',
+            };
+            this.data.panelData = {
+                scoreId: scoreId,
+                score: null,
+                video: null,
+                renderedreplayurl: '',
+                progress: 0,
+            };
+        });
+    },
+    methods: {
+        handleUpdate(newData) {
+            // handle the new data...
+            this.data = newData;
+        },
+        close: function() {
+            this.show = false;
+        },
+    },
+    computed: {}
+});
+vue.component('beatmap-panel', { 
+    props: ['beatmap'],
+    template: `
+    
+    `,
+    methods: {
+        handleUpdate(newData) {
+            // handle the new data...
+            this.data = newData;
+        }
+    },
+    computed: {},
+    created() {
+    },
+    mounted() {
+    },
+    updated() {
+    },
+    destroyed() {
+    },
+    watch: {
+    },
+    components: {
+    },
+    directives: {
+    },
+    filters: {
+    },
+    mixins: [],
+    extends: {},
+    beforeCreate() {
+    }
+});
