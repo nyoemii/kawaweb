3a222f3d3d02902ba3d8b02d2aefe376471f578d
ロキ | Loki <goingloki@gmail.com>
2024-01-06 19:29:49 -0500

    _

--- blueprints/frontend.py
+++ blueprints/frontend.py
@@ -419,7 +419,7 @@ async def profile_select(id):
     user_data = await glob.db.fetch(
         'SELECT users.name, users.safe_name, users.id, users.priv, users.country, user_customisations.hue '
         'FROM users '
-        'LEFT JOIN user_customisations ON users.id = user_customisations.user_id '
+        'LEFT JOIN user_customisations ON users.id = user_customisations.userid '
         'WHERE users.safe_name = %s OR users.id = %s LIMIT 1',
         [utils.get_safe_name(id), id]
     )
