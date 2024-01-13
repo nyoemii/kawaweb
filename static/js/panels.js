04e92f0ab87d50f483fc28ff15e745e1c3b1c0e9
Austin Steyer <asteyer11@gmail.com>
2024-01-13 13:28:40 -0700

    Docs: Add osu!rx DL

--- static/js/panels.js
+++ static/js/panels.js
@@ -713,7 +713,7 @@ new Vue({
                         </div>
                         <div class="doc-block" v-if="page === 'osu!rx'">
                             <div class="doc-notice">
-                                <a class="button dl" href=""><i class="fas fa-download"></i>  Download</a>
+                                <a class="button dl" href="https://www.mpgh.net/forum/showthread.php?t=1538659"><i class="fas fa-download"></i>  Download</a>
                             </div>
                             <div class="doc-section">
                                 <h3><i class="fas fa-exclamation-circle"></i> osu!rx Hack:</h3>
