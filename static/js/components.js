854df79fd9a70b633a5a254c06d1263cbf2b0782
Austin Steyer <asteyer11@gmail.com>
2024-02-10 20:45:24 -0700

    DeHardcode Icon fas fa-

--- static/js/components.js
+++ static/js/components.js
@@ -46,7 +46,7 @@ Vue.component('badge', {
   <span data-popup="badge">
     <div v-if="type === 0" class="badge"  :style="'background-color: hsl(' + badge.styles.color + ', 20%, 30%); color: hsl(' + badge.styles.color + ', 100%, 80%);  border: 1px solid hsl(' + badge.styles.color + ', 40%, 35%);'" @mouseover="showPanel = true" @mouseleave="showPanel = false">
       <span class="icon" v-if="badge.styles.icon">
-        <i v-bind:class="'fas fa-' + badge.styles.icon"></i>
+        <i v-bind:class="'' + badge.styles.icon"></i>
       </span>
       <span class="badge-name">
         {{ badge.name }}
